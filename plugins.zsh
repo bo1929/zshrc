98408c9ab8540c7355d43088703d3353529f19e6
Ali Osman Berk Şapcı <asapci@ucsd.edu>
2026-08-17 13:51:49 -0700

    antigen zshrc

--- plugins.zsh
+++ plugins.zsh
@@ -6,6 +6,18 @@ fi
 ADOTDIR="${HOME}/.config/zsh/antigen"
 source ${PATH_ANTIGEN}
 
+typeset -ga ANTIGEN_CHECK_FILES
+ANTIGEN_CHECK_FILES=()
+if [[ -f "${ZDOTDIR:-$HOME/.config/zsh}/.zshrc" ]]; then
+  ANTIGEN_CHECK_FILES+=("${ZDOTDIR:-$HOME/.config/zsh}/.zshrc")
+fi
+if [[ -f "${ZDOTDIR:-$HOME/.config/zsh}/plugins.zsh" ]]; then
+  ANTIGEN_CHECK_FILES+=("${ZDOTDIR:-$HOME/.config/zsh}/plugins.zsh")
+fi
+if [[ -f "${HOME}/.zshrc" ]]; then
+  ANTIGEN_CHECK_FILES+=("${HOME}/.zshrc")
+fi
+
 # == zsh-users Plugins ==
 # Autosuggestions.
 antigen bundle zsh-users/zsh-autosuggestions
